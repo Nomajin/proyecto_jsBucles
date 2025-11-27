84ab8d384ad42d3aaf285fb4f245d7edb4046661
Nomajin <noahmartos@gmail.com>
2025-11-27 20:13:54 +0100

    Update ejercicio 2.4.js

--- Ejercicios_bucles_2/ejercicio 2.4.js
+++ Ejercicios_bucles_2/ejercicio 2.4.js
@@ -1,37 +1,26 @@
 //4. Se pide representar el algoritmo que nos calcule la suma de los N primeros números pares. 
 // Es decir, si insertamos un 5, tendremos la suma de 2+4+6+8+10.
-
 const prompt = require('prompt-sync')();
 
-let n = prompt ('Introduzca n: ') //devuelve string
-n = Number (n);
+const N = Number (prompt('Que numero eliges? '));
+let Acumulador = 0;
+let contador = 0;
 
-if (!Number.isNaN(n)) {
-    // n es number
 
-    let contador = 0;
+    while (contador <= N*2) {
 
-    let acumulador = 1;
-
-    while (contador < n) {
-
-    console.log ( ` contador = ${contador} `)
-    console.log ( ` contador = ${ acumulador}`)
 
         if (contador % 2 === 0) {
             //contador es par
 
-            console.log (`contador par`);
+            Acumulador = Acumulador + contador;
 
-            acumulador = acumulador + contador;
+            console.log (`Numero Par ${Acumulador}`);
+
         }
 
-        ++contador;
+     contador++;
         
     }
 
-    console.log ( ` Total de nº1 pares ${acumulador} `)
-
-} else {
-    console.log(`n no es un numero`)
-}
+    console.log ( ` Total de nº1 pares ${Acumulador} `)
